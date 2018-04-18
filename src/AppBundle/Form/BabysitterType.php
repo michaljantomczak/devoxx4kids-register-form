c7aa0c1d48bd95ecf0e4b48a59933af51f807302
Michał Tomczak <michal.tomczak@newaxis.pl>
2018-04-18 21:30:40 +0200

    updated terms

--- src/AppBundle/Form/BabysitterType.php
+++ src/AppBundle/Form/BabysitterType.php
@@ -72,14 +72,14 @@ class BabysitterType extends AbstractType
                 ]
             ])
             ->add('allowTerms',null,[
-                'label'=>'Wyrażam zgodę na przetwarzanie moich danych osobowych przez Fundację Coder Dojo Polska z siedzibą w Zambrowie (ul. Papieża Jana Pawła II 12A/34, 18-300 Zambrów), w celu udziału w warsztatach programistycznych dla dzieci Devoxx4Kids.',
+                'label'=>'Wyrażam zgodę na przetwarzanie moich danych osobowych przez Stowarzyszenie Polska Grupa Użytkowników Pythona, ul. Madalińskiego 106, 02-506 Warszawa, w celu udziału w warsztatach programistycznych dla dzieci Devoxx4Kids.',
                 'required'=>true,
                 'constraints'=>[
                     new NotBlank(),
                 ]
             ])
             ->add('allowMarketing',null,[
-                'label'=>'Wyrażam zgodę na przesyłanie mi przez  Fundację Coder Dojo Polska za pomocą środków komunikacji elektronicznej informacji o przyszłych warsztatach (np. newsletterów).',
+                'label'=>'Wyrażam zgodę na przesyłanie mi przez Stowarzyszenie Polska Grupa Użytkowników Pythona, ul. Madalińskiego 106, 02-506 Warszawa za pomocą środków komunikacji elektronicznej informacji o przyszłych warsztatach (np. newsletterów).',
             ])
             ->add('countChild',ChoiceType::class,[
                 'placeholder'=>'Select...',
